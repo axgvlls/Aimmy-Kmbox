67b046fece131a0d3bfe64cf10b76b89c5bc3f29
Travis Boyles <trav.boyles@gmail.com>
2024-10-29 07:15:19 -0400

    Add project files.

---
+++ Aimmy2/MouseMovementLibraries/KMBoxSupport/KMbox.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Windows;
+using Visuality;
+
+namespace MouseMovementLibraries.KMBoxSupport
+{
+    internal class KMBox
+    {
+        #region KMBox Variables
+
+
+        [DllImport("KMBoxDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool ConnectKMBOX();
+
+        [DllImport("KMBoxDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void StartListening(int vkCode);
+
+        [DllImport("KMBoxDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Move(int x, int y);
+
+        [DllImport("KMBoxDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Up(int key);
+        
+        [DllImport("KMBoxDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Down(int key);
+
+        #endregion KMBox Variables
+
+    }
+}
